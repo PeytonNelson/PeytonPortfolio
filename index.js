a658126d85fbb6d9ef85cfe14c60dccb3ac76d98
PeytonNelson <peytonnelson3@gmail.com>
2022-09-21 17:09:26 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+var strPhrases = {
+    strings: ["Software Engineer", "Developer", "Problem Solver"],
+    typeSpeed: 80,
+    backSpeed: 65,
+    loop: true,
+  };
+  
+  var typed = new Typed('#txtPhrases', strPhrases);
